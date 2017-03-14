4dc9d2edf95bbd14f8455f2e50edefc79a92fa56
Xander <xander.groeneveld@gmail.com>
2017-03-14 23:37:11 +1100

    Redis updates for heroku deploy

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,8 +1,4 @@
 if !Rails.env.test?
   configs = YAML.load_file("./config/redis.yml")[Rails.env]
-  Rails.application.config.redis = Redis.new(
-    :host => configs["host"],
-    :port => configs["port"],
-    :db => configs["db"]
-  )
+  Rails.application.config.redis = Redis.new(url: configs[:url])
 end
